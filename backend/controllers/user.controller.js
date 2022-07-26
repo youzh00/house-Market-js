45a6f95ebfbfda924e7b950a39111a38d251e1aa
Youssef Zahi <youssef.zahi2016@gmail.com>
2022-07-26 20:07:29 +0100

    get user by its id added

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,7 +1,7 @@
 const UserModel = require("../models/user.model");
 
 //sign up an user to database
-//path: /users
+//path: /users/register
 // public
 const createUserAccount = async (req, res) => {
   try {
@@ -13,6 +13,39 @@ const createUserAccount = async (req, res) => {
   }
 };
 
+//Login an user
+//path: /users/login
+//public
+const loginUser = async (req, res) => {
+  try {
+    const user = await UserModel.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    res.status(200).send({ user });
+  } catch (error) {
+    res.status(500).send(`Error: ${error}`);
+  }
+};
+
+//get user by id from database
+//path: /users/:id
+// private : to admin
+const getUserById = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const user = await UserModel.findById(id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
 module.exports = {
   createUserAccount,
+  loginUser,
+  getUserById,
 };
--- backend/routes/user.router.js
+++ backend/routes/user.router.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const router = new express.Router();
-const { createUserAccount } = require("../controllers/user.controller");
+const {
+  createUserAccount,
+  loginUser,
+  getUserById,
+} = require("../controllers/user.controller");
 
 router.route("/register").post(createUserAccount);
+
+router.route("/login").post(loginUser);
+
+router.route("/:id").get(getUserById);
 
 module.exports = router;
