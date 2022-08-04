79308aa119ec1a99c941f8c5c9652d7d9a06f9a4
Youssef Zahi <youssef.zahi2016@gmail.com>
2022-08-04 11:24:11 +0100

    get houses by queries

--- backend/controllers/house.controller.js
+++ backend/controllers/house.controller.js
@@ -4,15 +4,11 @@ const HouseModel = require("../models/house.model");
 //private:to connected users
 //path:/houses
 const createHouse = async (req, res) => {
-  console.log("Out of createHouse try catch");
-
   try {
-    console.log("Inside of createHouse try catch");
     const house = new HouseModel({
       ...req.body,
       author: req.user._id,
     });
-    console.log(house);
     await house.save();
     res.send(house);
     console.log("house saved successfully");
@@ -22,5 +18,49 @@ const createHouse = async (req, res) => {
   }
 };
 
-//------------------------------------------- Exports-------------------------------------------//
-module.exports = { createHouse };
+//get tasks from database using queries
+//public: For everyone
+//path:
+//    /houses?forRent=true
+//    /houses?forSale=true
+//    /houses?city=marrakech
+//    /houses?limit=10&skip=0
+//    /houses?sortBy=createdAt:desc
+const getAllHouses = async (req, res) => {
+  const match = {};
+  const sort = {};
+  if (req.query.forRent) {
+    match.forRent = req.query.forRent === "true";
+  }
+  if (req.query.forSale) {
+    match.forSale = req.query.forSale === "true";
+  }
+  if (req.query.city) {
+    match.city = req.query.city;
+  }
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(":");
+    sort[parts[0]] = parts[1] === "desc" ? -1 : 1;
+  }
+  try {
+    await req.user.populate({
+      path: "houses",
+      match,
+      options: {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip),
+        sort,
+      },
+    });
+    const houses = req.user.houses;
+    if (houses.length === 0) {
+      return res.status(404).send();
+    }
+    res.status(200).send(houses);
+  } catch (error) {
+    res.status(500);
+    throw new Error(`Error: ${error}`);
+  }
+};
+//-------------------------------------------- Exports-------------------------------------------//
+module.exports = { createHouse, getAllHouses };
--- backend/routes/house.router.js
+++ backend/routes/house.router.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = new express.Router();
 const isConnected = require("../middlewares/auth.middleware");
-const { createHouse } = require("../controllers/house.controller");
+const {
+  createHouse,
+  getAllHouses,
+} = require("../controllers/house.controller");
 //!--------------------------------Routes----------------------------------//
-router.route("/").post(isConnected, createHouse);
+router.route("/").post(isConnected, createHouse).get(isConnected, getAllHouses);
 
 module.exports = router;
