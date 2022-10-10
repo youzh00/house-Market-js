21d7b55dd38fcd9791a2b92eefc12450e33ec3b4
Youssef Zahi <youssef.zahi2016@gmail.com>
2022-10-10 17:02:49 +0100

    backend : update auth  functionalities v2

--- backend/routes/auth.router.js
+++ backend/routes/auth.router.js
@@ -3,13 +3,15 @@ const router = new express.Router();
 const {RegisterUser}=require('../controllers/register.controller')
 const {LoginUser}=require('../controllers/login.controller')
 const {LogoutUser}=require('../controllers/logout.controller')
-const {isConnected2} = require("../middlewares/auth2.middleware");
+const {handleRefreshToken}=require('../controllers/refreshToken.controller')
+// const {isConnected2} = require("../middlewares/auth2.middleware");
 
 
 
 router.route("/register").post(RegisterUser);
 router.route("/login").post(LoginUser);
-router.route("/logout").post(isConnected2,LogoutUser);
+router.route("/logout").post(LogoutUser);
+router.route("/refresh").get(handleRefreshToken);
 
 
 module.exports = router;
