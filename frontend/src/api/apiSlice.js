04969122683232751825aea5ec718372058eaa26
Youssef Zahi <youssef.zahi2016@gmail.com>
2022-10-12 14:47:45 +0100

    create auth slice

---
+++ frontend/src/api/apiSlice.js
@@ -0,0 +1,8 @@
+import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+
+import { setCredentials,logOut  } from '../features/auth/authSlice'
+
+
+const baseQuery=fetchBaseQuery({
+    baseUrl: 'http://localhost:3000'
+})
